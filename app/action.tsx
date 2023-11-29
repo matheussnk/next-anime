995dcb5d0be4470bdbf98f6781e0aa21e113e70b
Matheus Mendonça Lima <matheus.mendonca@apoioprodesp.sp.gov.br>
2023-11-29 20:47:29 -0300

    fixed bug scroll

--- app/action.tsx
+++ app/action.tsx
@@ -2,7 +2,7 @@
 
 import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
 
-const MAX_LIMIT = 8;
+const MAX_LIMIT = 12;
 
 export async function fetchAnime(page: number) {
   const response = await fetch(
