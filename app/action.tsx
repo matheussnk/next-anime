459637c5dc6ec8e2d3bd7ac8f7b9b204b390d64a
Matheus Mendonça Lima <matheus.mendonca@apoioprodesp.sp.gov.br>
2023-11-29 20:14:43 -0300

    up branch fix

--- app/action.tsx
+++ app/action.tsx
@@ -1,4 +1,7 @@
 "use server";
+
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+
 const MAX_LIMIT = 8;
 
 export async function fetchAnime(page: number) {
@@ -8,5 +11,7 @@ export async function fetchAnime(page: number) {
 
   const data = await response.json();
 
-  return data;
+  return data.map((anime: AnimeProp, index: number) => (
+    <AnimeCard key={anime.id} anime={anime} index={index} />
+  ));
 }
